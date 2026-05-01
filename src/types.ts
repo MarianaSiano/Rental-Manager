d5e71ea695200f493d43694e455f740a991ed623
MarianaSiano <marianasianop@gmail.com>
2026-05-01 18:00:05 -0300

    types.ts concluido

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,23 @@ export interface User {
     email: string;
     role: 'admin' | 'user';
 }
+
+export interface Product {
+    id: string;
+    name: string;
+    type: 'equipamento' | 'epi' | 'tecnologia' | 'outros';
+    category: string;
+    status: 'available' | 'rented' | 'maintenance';
+}
+
+export interface Rental {
+    id: string;
+    userId: string;
+    productId: string;
+    startTime: string;
+    endTime: string;
+    depositPaid: boolean;
+    remainingPaid: boolean;
+    status: 'active' | 'returned' | 'overdue';
+    price: number;
+}
